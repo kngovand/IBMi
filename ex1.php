3cd6ec8e13f6c7e47f46704f00b5b462b5f06bed
K Duc <kngovand@gmail.com>
2020-09-30 10:53:55 -0600

    ex1

---
+++ ex1.php
@@ -0,0 +1,19 @@
+<html>
+<header><title>ex1</title>
+</header>
+<body>
+    <form action="ex1.php" method="POST">
+    <?php 
+    {
+        if(isset($_POST['myname'])) {
+            $displayName = $_POST['myname'];
+            echo "hello $displayName";
+        }
+    } 
+    ?>
+
+    <input type="text" name="myname" value ="enter name here... ">
+    <input type="submit" name="Submit">
+    </form>
+</body>
+</html>
