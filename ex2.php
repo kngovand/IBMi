b9c0ae00630d89a23abee016d9facd973e1eaf92
K Duc <kngovand@gmail.com>
2020-09-30 11:09:54 -0600

    ex2

---
+++ ex2.php
@@ -0,0 +1,25 @@
+<html>
+<header><title>ex2</title>
+</header>
+<body>
+    <h2>Enter number: </h2>
+    <form action="ex2.php" method="POST">
+    <?php 
+    {
+        if(isset($_POST['mynum'])) {
+            $favoritenum = $_POST['mynum'];
+            if($favoritenum == 17) {
+                echo "17 is my favorite number too! \n";
+            }
+            else {
+                echo "$favoritenum is not my favorite number. \n";
+            }
+        }
+    } 
+    ?>
+
+    <input type="number" name="mynum" value ="enter num here... ">
+    <input type="submit" name="Submit">
+    </form>
+</body>
+</html>
