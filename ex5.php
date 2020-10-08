c740b71acdba331ebd2f63c67531026c6150dc0b
K Duc <kngovand@gmail.com>
2020-10-07 19:47:37 -0600

    more exercises rpg to php

---
+++ ex5.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ex5</title>
+</head>
+<body>
+    <?php
+       $a = 1;
+       $b = 2;
+       
+       if($a == 1 or $b == 1) {
+            echo "Condition one true";
+       }
+       elseif($a == 1 or $b == 2) {
+            echo "Condition two true";
+       }
+    ?>
+    
+</body>
+</html>
---
+++ ex7.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ex7</title>
+</head>
+<body>
+    <?php
+        $count = 7;
+        while ($count < 40) {
+            echo $count;
+            $count += $count / 2;
+        }
+    ?>
+</body>
+</html>
