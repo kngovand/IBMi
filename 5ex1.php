9713d7110c86b85da5a3a184d609dfd6d4d81071
K Duc <kngovand@gmail.com>
2020-10-13 11:56:46 -0600

    new folder

--- 5ex1.php
+++ 5ex1.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>5ex</title>
+    <title>I/O Demo</title>
 </head>
 <body>
     <?php
+    
     ?>
 </body>
 </html>
