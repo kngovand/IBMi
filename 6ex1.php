0a1f2d2fef718df9ed1d459f4667a8adee8ceca8
K Duc <kngovand@gmail.com>
2020-10-13 13:57:41 -0600

    object-oriented problems

---
+++ 6ex1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>what's wrong? pt1</title>
+</head>
+<body>
+    <?php
+/*         interface User {
+            #interfaces can't have body definition
+            public function getName() {
+                return 'John';
+            }
+        }
+
+        $user = new User();
+        echo $user->getName(); */
+    ?>
+    
+</body>
+</html>
---
+++ 6ex2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>what's wrong? pt2</title>
+</head>
+<body>
+    <?php
+        abstract class User {
+            public function getName() {
+                return 'John';
+            }
+        }
+
+        $user = new User();
+        echo $user->getName();
+    ?>
+    
+</body>
+</html>
