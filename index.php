2fa943b029d8cf150f715ac681bdd4342f594bae
K Duc <kngovand@gmail.com>
2020-10-13 14:08:03 -0600

    better routing for php/index.html for navigation

--- index.php
+++ index.php
@@ -6,7 +6,12 @@
     <title>PHP Exercises</title>
 </head>
 <body>
-<p>Just some PHP exercises.</p>
-    
+    <h2>Just some PHP exercises.</h2>
+    <a href="chapter1">Chapter 1</a><br>
+    <a href="chapter2">Chapter 2</a><br>
+    <a href="chapter3">Chapter 3</a><br>
+    <a href="chapter4">Chapter 4</a><br>
+    <a href="chapter5">Chapter 5</a><br>
+    <a href="chapter6">Chapter 6</a><br> 
 </body>
 </html>
