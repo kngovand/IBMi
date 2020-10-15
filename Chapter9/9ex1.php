e3f5351967fe60e76e69cfbc88cf0ba8205bc1b7
K Duc <kngovand@gmail.com>
2020-10-15 12:32:19 -0600

    Cookie/Session scripts incoming

---
+++ Chapter9/9ex1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cookies/Sessions 1</title>
+</head>
+<body>
+    
+</body>
+</html>
